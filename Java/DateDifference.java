7e8c62dc4f5ca7e8f95c10b281d4a8955dd8cc78
Adi-Pandit <adityapandit705@gmail.com>
2023-02-01 09:16:16 +0530

    name change

--- Java/DateDifference.java
+++ Java/DateDifference.java
@@ -1,5 +1,6 @@
-import java.io.*;
-import java.util.Hashtable;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class DateDifference
 {
@@ -10,45 +11,22 @@ public class DateDifference
         else    
             return false;
     }
-    public static void main(String args[]) throws IOException
+
+    public static boolean DateValidation(int day, int month, int year)
     {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int arrOfDay[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+        if(LeapYear(year))
+            arrOfDay[1] = 29;
+        if(year>0 && month>0 && month<=12 && day>0 && day<=arrOfDay[month-1])
+            return true;
+        else 
+            return false;
+    }
 
-        System.out.println("Enter details of start date: ");
-        System.out.print("Enter Day: ");
-        int sDay = Integer.parseInt(br.readLine());
-        System.out.print("Enter Month: ");
-        int sMonth = Integer.parseInt(br.readLine());
-        System.out.print("Enter Year: ");
-        int sYear = Integer.parseInt(br.readLine());
-        System.out.print("Start date: "+sDay+"/"+sMonth+"/"+sYear);
-
-        System.out.println("\n\nEnter details of end date: ");
-        System.out.print("Enter Day: ");
-        int eDay = Integer.parseInt(br.readLine());
-        System.out.print("Enter Month: ");
-        int eMonth = Integer.parseInt(br.readLine());
-        System.out.print("Enter Year: ");
-        int eYear = Integer.parseInt(br.readLine());
-        System.out.print("End date: "+eDay+"/"+eMonth+"/"+eYear);
-
-        Hashtable<Integer,Integer> monthDays = new Hashtable<Integer,Integer>();
-        monthDays.put(1,31);
-        monthDays.put(2,28);
-        monthDays.put(3,31);
-        monthDays.put(4,30);
-        monthDays.put(5,31);
-        monthDays.put(6,30);
-        monthDays.put(7,31);
-        monthDays.put(8,31);
-        monthDays.put(9,30);
-        monthDays.put(10,31);
-        monthDays.put(11,30);
-        monthDays.put(12,31);
-
-        System.out.println("\nMonth and days: "+monthDays);
+    public static void DifferenceBetweenTwoDates(int sDay, int sMonth, int sYear, int eDay, int eMonth, int eYear)
+    {
+        int monthDays[] = {31,28,31,30,31,30,31,31,30,31,30,31};
         int dayDifference = 0;
-
         if(eYear==sYear)
         {  
             if(eMonth==sMonth)
@@ -72,11 +50,11 @@ public class DateDifference
             }
             else 
             {
-                dayDifference += monthDays.get(sMonth)-sDay;
+                dayDifference += monthDays[sMonth-1]-sDay;
                 sMonth++;
                 while(sMonth<eMonth)
                 {
-                    dayDifference += monthDays.get(sMonth);
+                    dayDifference += monthDays[sMonth-1];
                     sMonth++;
                 }
                 if(LeapYear(eYear) && eMonth>2)
@@ -90,13 +68,13 @@ public class DateDifference
         }
         else 
         {
-            dayDifference += monthDays.get(sMonth)-sDay;
+            dayDifference += monthDays[sMonth-1]-sDay;
             if(LeapYear(sYear) && sMonth<=2)
                 dayDifference += 1;
             sMonth++;
             while(sMonth<=12)
             {
-                dayDifference += monthDays.get(sMonth);
+                dayDifference += monthDays[sMonth-1];
                 sMonth++;
             }
             sYear++;
@@ -104,13 +82,48 @@ public class DateDifference
             int month=1;
             while(month<eMonth)
             {
-                dayDifference += monthDays.get(month);
+                dayDifference += monthDays[month-1];
                 month++;
             }
             dayDifference += eDay;
             if(LeapYear(eYear) && month>2)
                 dayDifference += 1;
         }
-        System.out.println("Days: "+dayDifference);
+        System.out.println("\nDifference Between Two Dates: "+dayDifference);
+    }
+
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter details of start date: ");
+        System.out.print("Enter Day: ");
+        int sDay = Integer.parseInt(br.readLine());
+        System.out.print("Enter Month: ");
+        int sMonth = Integer.parseInt(br.readLine());
+        System.out.print("Enter Year: ");
+        int sYear = Integer.parseInt(br.readLine());
+        if(!DateValidation(sDay, sMonth, sYear))
+        {
+            System.out.println("\nStart date: "+sDay+"/"+sMonth+"/"+sYear+" is invalid");
+            System.exit(0);
+        }
+        System.out.print("Start date: "+sDay+"/"+sMonth+"/"+sYear);
+
+        System.out.println("\n\nEnter details of end date: ");
+        System.out.print("Enter Day: ");
+        int eDay = Integer.parseInt(br.readLine());
+        System.out.print("Enter Month: ");
+        int eMonth = Integer.parseInt(br.readLine());
+        System.out.print("Enter Year: ");
+        int eYear = Integer.parseInt(br.readLine());
+        if(!DateValidation(eDay, eMonth, eYear))
+        {
+            System.out.println("\nEnd date: "+eDay+"/"+eMonth+"/"+eYear+" is invalid");
+            System.exit(0);
+        }
+        System.out.print("End date: "+eDay+"/"+eMonth+"/"+eYear);
+
+        DifferenceBetweenTwoDates(sDay,sMonth,sYear,eDay,eMonth,eYear);
     }
 }
